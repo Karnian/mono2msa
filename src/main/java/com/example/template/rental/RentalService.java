9ef848269137e8da6cac53790a61a563baecd1e0
Hyung Ho Kim <khh9201@ajou.ac.kr>
2020-10-29 05:17:52 +0900

    t2

--- src/main/java/com/example/template/rental/RentalService.java
+++ src/main/java/com/example/template/rental/RentalService.java
@@ -1,8 +1,10 @@
-package com.example.template.product;
+package com.example.template.rental;
 
-public interface ProductService {
-    void decreaseStock(Long productId, int quantity);
-    void increaseStock(Long productId, int quantity);
-    Product getProductById(Long id);
-    Product save(String data);
+import org.springframework.stereotype.Service;
+
+@Service
+public interface RentalService {
+    Rental getRentalById(long rentalId);
+    Rental save(Rental data);
+    Rental save(String data);
 }
